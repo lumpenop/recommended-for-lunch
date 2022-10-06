98d696625656606565138ed2efde98132e88bc89
lumpenop <swk5373@gmail.com>
2022-10-06 23:59:11 +0900

    feat: KeyboardAvoidingView 추가중..

--- App.tsx
+++ App.tsx
@@ -9,14 +9,21 @@
  */
 
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
+import {
+  StatusBar,
+  useColorScheme,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Platform,
+} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RootRoutes from './src/routes/root.routes';
 
 const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -24,12 +31,15 @@ const App = () => {
 
   return (
     <SafeAreaProvider style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      {/*<RootNavigation />*/}
-      <RootRoutes />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        {/*<RootNavigation />*/}
+        <RootRoutes />
+      </KeyboardAvoidingView>
     </SafeAreaProvider>
   );
 };
